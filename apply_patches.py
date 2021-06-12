a231db3e87b58469e78e158dd0f6641efa8b0124
Artemii Bigdan <artem@bigdan.in>
2021-06-13 01:20:36 +0300

    Link submodule and script improvements

--- apply_patches.py
+++ apply_patches.py
@@ -3,18 +3,18 @@ from os.path import join, isfile
 Import("env")
 
 PROJECT_LIBDEPS_DIR = env["PROJECT_LIBDEPS_DIR"]
+PROJECT_DIR = env["PROJECT_DIR"]
 patchflag_path = join(PROJECT_LIBDEPS_DIR, ".patching-done")
 
 # patch file only if we didn't do it before
 if not isfile(patchflag_path):
     original_file = join(PROJECT_LIBDEPS_DIR, "esp32doit-devkit-v1", "AsyncTCP", "src", "AsyncTCP.cpp")
-    patch_file = join("patches", "1-AsyncTCP-xQueueCreate-size.patch")
+    patch_file = join(PROJECT_DIR, "patches", "1-AsyncTCP-xQueueCreate-size.patch")
 
     assert isfile(original_file) and isfile(patch_file)
 
-    print("Partching ${original_file} with ${patch_file}")
+    print(f"Partching {original_file} with {patch_file}")
     env.Execute("patch %s %s" % (original_file, patch_file))
-    # env.Execute("touch " + patchflag_path)
 
 
     def _touch(path):
---
+++ link_static.py
@@ -0,0 +1,15 @@
+from genericpath import isdir, isfile
+from os import symlink
+from os.path import join
+
+Import("env")
+
+PROJECT_DATA_DIR = env["PROJECT_DATA_DIR"]
+PROJECT_DIR = env["PROJECT_DIR"]
+
+src = join(PROJECT_DIR, "control-panel-webpage", "stub-webserver", "static")
+dst = join(PROJECT_DATA_DIR, "static")
+
+if not isfile(dst) and not isdir(dst):
+    print(f"Creating symbolic link {src} -> {dst}")
+    symlink(src, dst)
